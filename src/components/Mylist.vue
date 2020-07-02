88c91b9469c37f34d466610ffcfce542724cc57b
aso1801003 <1801003@s.asojuku.ac.jp>
2020-07-02 05:47:34 +0000

    done

---
+++ src/components/Mylist.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h3>{{ message }}</h3>
+        <div>
+            <input type="text" v-model="input">
+            <button v-on:click="doAction">クリック</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Mylist',
+    props:{
+        title: String,
+    },
+    data:function(){
+        return {
+            message:'メモを入力してください',
+            input:'input here.',
+        };
+    },
+    methods:{
+        doAction: function(){
+            this.$emit('result-event',this.input);
+        }
+    }
+}
+</script>
+
+<style>
+div{
+    margin:0px;
+    padding:0px;
+    text-align:center;
+}
+p{
+    margin:0px;
+    color:#666;
+    font-size:16pt;
+}
+</style>
